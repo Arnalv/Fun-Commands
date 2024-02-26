2884605830b6c6f8a90dc52ca32277e5e26e3b06
Arnalv <arnavarora751@gmail.com>
2024-02-26 00:04:21 -0600

    change funcmd.py

--- funcmd.py
+++ funcmd.py
@@ -3,21 +3,29 @@ import sys
 
 parser = ap.ArgumentParser()
 parser.add_argument("-a", "--add-item", help = "add a new command to the list", action="store_true")
+parser.add_argument("-c", "--clear", help = "add a new command to the list", action="store_true")
 args = parser.parse_args()
 
 def main():
     try:
         if args.add_item:
             new_command = input("What do you want to add?:")
-            commands = open("list.txt", "a")
+            commands = open("/usr/lib/funlist.txt", "a")
             commands.write("- " + new_command + "\n")
             commands.close()
+        elif args.clear:
+            try:
+                commands = open("/usr/lib/funlist.txt", "w")
+                commands.write("")
+                commands.close()
+            except:
+                print("an error occured, try running this command as sudo")
         else:
-            commands = open("list.txt", "r")
-            print(commands.read())
+            commands = open("/usr/lib/funlist.txt", "r")
+            print("Fun Commands List\n\n" + commands.read())
             commands.close()
     except:
-        temp_list = open("list.txt", "w")
+        temp_list = open("/usr/lib/funlist.txt", "w")
         temp_list.close()
 if __name__ == "__main__":
     main()
